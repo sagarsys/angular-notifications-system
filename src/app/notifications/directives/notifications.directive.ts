a5f20e3239c74e4a6a0a00f7a7ed06e81a877bed
sagar <sagar_sys@live.com>
2018-08-11 10:39:28 +0400

    create the notifications directive

---
+++ src/app/notifications/directives/notifications.directive.ts
@@ -0,0 +1,47 @@
+import { ComponentFactoryResolver, ComponentRef, Directive, ViewContainerRef } from '@angular/core';
+import { UnsubscribeHelper } from '../../shared/helpers/Unsubscribe.helper';
+import { NotificationsManagerService } from '../services/notifications-manager.service';
+import { NotificationsComponent } from '../components/notifications.component';
+import { NOTIFICATION_ANIM_DURATION, NotificationsConfigModel } from '../models/notification-config.model';
+
+@Directive({ selector: '[appNotifications]' })
+export class NotificationsDirective extends UnsubscribeHelper {
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private factoryResolver: ComponentFactoryResolver,
+    private notificationsManagerService: NotificationsManagerService,
+  ) {
+    super();
+  }
+
+  create(
+    notificationsComponent: { new(...args: any[]): NotificationsComponent },
+    data: NotificationsConfigModel = null,
+  ): ComponentRef<NotificationsComponent> {
+    // remove any previous instance
+    this.viewContainerRef.clear();
+    // resolve and create component
+    const notificationsFactory = this.factoryResolver.resolveComponentFactory(NotificationsComponent);
+    const notificationsCompRef = this.viewContainerRef.createComponent(notificationsFactory);
+    // add data to notifications if present
+    if (data) {
+      notificationsCompRef.instance.data = Object.assign({}, notificationsCompRef.instance.data, data);
+    }
+    // event listener for close event
+    this.addSubscription(
+      notificationsCompRef.instance.close.subscribe((value: any) => {
+        // if user clicks on an action, pass the result before close
+        if (value) {
+          this.notificationsManagerService.result = value;
+        }
+        // wait for animation and destroy notification
+        setTimeout(() => {
+          notificationsCompRef.destroy();
+        }, NOTIFICATION_ANIM_DURATION);
+      })
+    );
+    return notificationsCompRef;
+  }
+
+}
