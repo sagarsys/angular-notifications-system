6f52845ac035909ed97cf28060608d6696a4f942
sagar <sagar_sys@live.com>
2018-08-11 12:46:45 +0400

    add notifications anchor to app + test basic notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { UnsubscribeHelper } from './shared/helpers/Unsubscribe.helper';
+import { NotificationsService } from './notifications/services/notifications.service';
+import { NotificationsConfigModel } from './notifications/models/notification-config.model';
+import { NotificationsDirective } from './notifications/directives/notifications.directive';
+import { NotificationsComponent } from './notifications/components/notifications.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends UnsubscribeHelper implements AfterViewInit {
+
+  @ViewChild(NotificationsDirective) notifications: NotificationsDirective;
+
+  constructor(private notificationsService: NotificationsService) {
+    super();
+  }
+
+  ngAfterViewInit() {
+    this.addSubscription(
+      this.notificationsService.isDisplayed$.subscribe((isDisplayed: boolean) => {
+        if (isDisplayed) {
+          this.addSubscription(
+            this.notificationsService.config$.subscribe((config: NotificationsConfigModel) => {
+              this.notifications.create(NotificationsComponent, config);
+            })
+          );
+        }
+      })
+    );
+  }
 
   onClick(event: MouseEvent) {
     console.log(event);
+    this.notificationsService.isDisplayed = true;
+    this.notificationsService.config = {
+      message: 'Success!',
+      message$: new Observable(),
+      type: 'success',
+      context: 'app',
+      duration: 15000
+    };
   }
 
 }
