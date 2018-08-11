72ff9517dbdf470aa74551c677fa5c5fc7a7b45e
sagar <sagar_sys@live.com>
2018-08-11 10:40:35 +0400

    register notifications component, directive & service in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NotificationsComponent } from './notifications/notifications.component';
+import { NotificationsComponent } from './notifications/components/notifications.component';
+import { NotificationsManagerService } from './notifications/services/notifications-manager.service';
+import { NotificationsDirective } from './notifications/directives/notifications.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotificationsComponent,
+    NotificationsDirective,
+  ],
+  entryComponents: [
     NotificationsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [NotificationsManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
