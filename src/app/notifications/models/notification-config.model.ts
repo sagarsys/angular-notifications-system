633a6a24264c72613cf0e6191d9f85c9a8b87bea
sagar <sagar_sys@live.com>
2018-08-13 07:49:57 +0400

    Make some properties optional in notifications config + remove unused ones

--- src/app/notifications/models/notification-config.model.ts
+++ src/app/notifications/models/notification-config.model.ts
@@ -5,19 +5,18 @@ export const NOTIFICATION_ANIM_DURATION = 200;
 
 export class NotificationsConfigModel {
   type: string;
-  message: string;
-  message$: Observable<string | any>;
+  message?: string;
+  message$?: Observable<string | any>; // can be used for dynamic messages --> takes priority over message if set
   duration: number;
-  context?: string;
   cssClasses?: string | string[];
   actions?: NotificationsActionModel[];
   zIndex?: number;
   position?: string;
-  additional?: any;
 }
 
 export class NotificationsActionModel {
   type: string;
   text: string;
-  text$: Observable<string | any>;
+  text$?: Observable<string | any>; // dynamic text --> priority over text if set
+  link?: string;
 }
